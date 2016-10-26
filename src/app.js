32bb0de97c1b61ec0ab72c8bbe5debf6cec5c638
nbkhope <ryumemaru@yahoo.com>
2016-10-25 17:42:40 -0700

    Add Provider to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 const App = () => {
   return (
-    <View>
-    </View>
+    <Provider store={createStore}>
+      <View>
+      </View>
+    </Provider>
   );
 };
 
