2956881d5baf98dd1695a65ef61c39fdd39640e8
nbkhope <ryumemaru@yahoo.com>
2016-10-25 18:13:44 -0700

    Add initial reducer

---
+++ src/reducers/LibraryReducer.js
@@ -0,0 +1,11 @@
+// You have to explicitly say you are importing .json file
+import data from './LibraryList.json';
+
+export default () => data;
+
+// export default function(state = [], action) {
+//   switch(action.type) {
+//
+//   }
+//   return state;
+// }
