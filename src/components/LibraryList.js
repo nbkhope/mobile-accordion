bcc777428a2b3cde06cf658a69acd8666d56616b
nbkhope <ryumemaru@yahoo.com>
2016-10-28 11:45:19 -0700

    Add ListView component to LibraryList and use ListItem component for renderRow

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from 'ListItem';
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = dataSource.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(library) {
+    return <ListItem library={library} />;
+  }
+
   render() {
     return (
-      <View>
-
-      </View>
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
